fdb3d8b1872c144d232a51400d4de1e1d217bdc8
Dharmik <hingudharmik4@gmail.com>
2021-11-19 20:50:08 +0530

    add new states

--- Text101/Assets/AdventureGame.cs
+++ Text101/Assets/AdventureGame.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,16 +8,51 @@ public class AdventureGame : MonoBehaviour
 {
   [SerializeField] Text textComponent; // [SerializeField] - To make this variable visible in unity as a component
                                        // Creates a variable of type 'Text' which comes from the UI Object of Unity
+  [SerializeField] State startingStory;
+  State currentState;
 
   // Start is called before the first frame update
   void Start()
   {
-    textComponent.text = ("Welcome to \"The Pass to Eldelvain\" "); //Access the 'text' property of textComponent
+    currentState = startingStory;
+    textComponent.text = currentState.GetStateStory(); //Access the 'text' property of textComponent
   }
 
   // Update is called once per frame
   void Update()
   {
+    ManageState();
+  }
 
+  private void ManageState()
+  {
+    var nextStates = currentState.GetNextStates();
+
+    if (nextStates.Length == 1)
+    {
+      if (Input.GetKeyDown(KeyCode.Return))
+      {
+        currentState = nextStates[0];
+      }
+    }
+    else
+    {
+      if (Input.GetKeyDown(KeyCode.Alpha1))
+      {
+        currentState = nextStates[0];
+      }
+      else if (Input.GetKeyDown(KeyCode.Alpha2))
+      {
+        currentState = nextStates[1];
+      }
+      else if (Input.GetKeyDown(KeyCode.Alpha3))
+      {
+        currentState = nextStates[2];
+      }
+
+    }
+
+
+    textComponent.text = currentState.GetStateStory();
   }
 }
